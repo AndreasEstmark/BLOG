4a46f67a6f45f8f4e869b4212d3ecf04d033b8fd
Anden <73430931+AndreasEstmark@users.noreply.github.com>
2021-09-26 12:00:42 +0200

    Started adding libraries

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 # First task is to perform anomaly detection in time series data
 
+# Using LSTM, import necessary dependecies
+from tensorflow import keras
+from sklearn.preprocessing import StandardScaler
+
+
+
+
+
